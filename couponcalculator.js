377f7595e1c3cd3c5881a78efb9f72b1b97fc0aa
Lynley Yamaguchi <lynleyyama@yahoo.com>
2019-01-01 21:32:34 -0800

    add percentage dropdown option

--- couponcalculator.js
+++ couponcalculator.js
@@ -4,11 +4,20 @@ let afterCoupons = document.getElementById('afterCoupons');
 let couponType = document.getElementById('couponType');
 
 document.getElementById('submit').addEventListener("click", function(){
+	let type = couponType.options[couponType.selectedIndex].value;
+
+	if(type === "dollar"){
+		document.getElementById('afterPrice').innerHTML = fullPrice.value - discount.value;
+	} else {
+		let discountValue = fullPrice.value * (discount.value/100);
+		document.getElementById('afterPrice').innerHTML = fullPrice.value - discountValue;
+	}
 	afterCoupons.classList.remove('hidden');
 	afterCoupons.classList.add('visible');
-	document.getElementById('afterPrice').innerHTML = fullPrice.value - discount.value;
+	
 	console.log(fullPrice.value);
 	console.log(discount.value);
+	console.log(couponType.options[couponType.selectedIndex].text);
 });
 
 
